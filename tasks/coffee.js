ce4d2002dc8ca3f8adaa3ac810ec9ceb5073b6c0
stkao05 <st.kao.05@gmail.com>
2015-03-06 15:52:33 +0800

    Run full build before incremental built

--- tasks/coffee.js
+++ tasks/coffee.js
@@ -6,34 +6,39 @@ var debug = require('gulp-debug');
 var coffee = require('gulp-coffee');
 var rename = require("gulp-rename");
 var newer = require('gulp-newer');
+var gulpif = require('gulp-if');
 var config = require('../config').coffee
 
 
+function buildCoffee(is_incremental_build) {
+  return function() {
+    gulp.src(config.src)
+      .pipe(gulpif(is_incremental_build, newer({ //only compile if source file is newer than dest file
+        dest: config.dist,
+        map: function(relativePath) {
+          return ".coffee." + path.parse(relativePath).name + ".js"
+        }
+      })))
+      .pipe(coffee())
+      .on("error", notify.onError({
+          title: "Coffee Error",  
+          subtitle: function(error) {
+            var filename = path.parse(error.filename).base;
+            return filename
+          },
+          message: "<%= error.name %>: <%= error.message %>"
+      }))
+      .pipe(rename({prefix: ".coffee."}))
+      .pipe(notify({title:"Coffee", message: "Coffee compile succeeded: <%= file.relative %>"}))
+      .pipe(gulp.dest(config.dist));
+  };
+}
 
 
-gulp.task('coffee', function() {
-  gulp.src(config.src)
-    .pipe(newer({ //only compile if source file is newer than dest file
-      dest: config.dist,
-      map: function(relativePath) {
-        return ".coffee." + path.parse(relativePath).name + ".js"
-      }
-    }))
-    .pipe(coffee())
-    .on("error", notify.onError({
-        title: "Coffee Error",  
-        subtitle: function(error) {
-          var filename = path.parse(error.filename).base;
-          return filename
-        },
-        message: "<%= error.name %>: <%= error.message %>"
-    }))
-    .pipe(rename({prefix: ".coffee."}))
-    .pipe(notify({title:"Coffee", message: "Coffee compile succeeded: <%= file.relative %>"}))
-    .pipe(gulp.dest(config.dist));
-});
-
+gulp.task('coffee', buildCoffee(false));
 
 gulp.task("watch:coffee", function() {
-  gulp.watch(config.src, ["coffee"])
+  buildCoffee(false)();
+  gulp.watch(config.src, buildCoffee(true))
 });
+
--- tasks/less.js
+++ tasks/less.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gulpif = require('gulp-if');
 var less = require('gulp-less');
 var rename = require("gulp-rename");
 var notify = require("gulp-notify");
@@ -7,23 +8,31 @@ var newer = require('gulp-newer');
 var config = require('../config').less
 
 
-gulp.task('less', function () {
-  return gulp.src(config.src)
-    .pipe(newer({ //only compile if source file is newer than dest file
-      dest: config.dist,
-      map: function(relativePath) {
-        return ".less." + path.parse(relativePath).name + ".css";
-      }
-    }))
-    .pipe(less())
-    .pipe(rename({
-      prefix: ".less."
-    }))
-    .pipe(gulp.dest(config.dist))
-    .pipe(notify("LESS compiled: <%= file.relative %>"));
-});
+function buildLess(is_incremental_build) {
+
+  return function() {
+    return gulp.src(config.src)
+      .pipe(gulpif(is_incremental_build, newer({ //only compile if source file is newer than dest file
+        dest: config.dist,
+        map: function(relativePath) {
+          return ".less." + path.parse(relativePath).name + ".css";
+        }
+      })))
+      .pipe(less())
+      .pipe(rename({
+        prefix: ".less."
+      }))
+      .pipe(gulp.dest(config.dist))
+      .pipe(notify("LESS compiled: <%= file.relative %>"));
+  }
+
+}
+
+
+gulp.task('less', buildLess(false));
 
 gulp.task("watch:less", function() {
-  gulp.watch(config.src, ["less"]);
+  buildLess(false)();
+  gulp.watch(config.src, buildLess(true))
 });
 
