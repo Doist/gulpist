3baa24a52ae4def28d2a8025ac87d19483cc7160
stkao05 <st.kao.05@gmail.com>
2015-03-06 14:14:17 +0800

    Fine tuning Browserify

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-  workDir: "../TeamCom",
+  //your project directory relative to doist_gulp
+  workDir: "../Todoist",
 
   less: {
     src: "teamcom/apps/web/static/css/**/app.less",
@@ -17,5 +18,19 @@ module.exports = {
     src: "./teamcom/apps/web/static/js/.coffee.app.js",
     dist: "./teamcom/apps/web/static/js/"
   },
+
+
+  coffee: {
+    src: "todoist/apps/app_platform/static/apps/app_platform/js/*.coffee",
+    dist: "todoist/apps/app_platform/static/apps/app_platform/js"
+  },
+
+  browserify: {
+    //browserify use different cwd, so path is configured differently
+    src: "./todoist/apps/app_platform/static/apps/app_platform/js/.coffee.app.js",
+    dist: "./todoist/apps/app_platform/static/apps/app_platform/js/bundle.js"
+  },
+  
+
 
 }
--- tasks/browserify.js
+++ tasks/browserify.js
@@ -9,135 +9,44 @@ var coffeereact = require('coffee-reactify');
 var coffeeify = require('coffeeify');
 var transform = require('vinyl-transform');
 var rename = require("gulp-rename");
-
 var config = require('../config').browserify
+
+
 
 gulp.task('browserify', function() {
 
+  destFileName = path.parse(config.dist).base
+  destDir = path.parse(config.dist).dir
+
+  //transform browserify bundler into vinyl stream
   var browserified = transform(function(filename) {
 
-    destFileName = "bundle.js"
-
     var bundler = browserify(filename, {
-      paths: ['../doist_gulp/node_modules'],
+      paths: ['../doist_gulp/node_modules'],  //TODO: remove this
       debug: true,
       cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
     });
     
-    //return bundler.bundle();
-
     var watcher  = watchify(bundler);
 
     return watcher
           .on('update', function () { // When any files update
-              console.log('Watchify detect updated. Rebundle...');
-              var updateStart = Date.now();
-
               watcher.bundle()
-                     .on("error", notify.onError({
-                        message: "<%= error.message %>",
-                        title: "Broserify Error"
-                     }))
-
-              console.log('Rebundle finished!', (Date.now() - updateStart) + 'ms');
+                     .pipe(source(destFileName))
+                     .pipe(gulp.dest(destDir))
+                     .pipe(notify({title: "Broserify - Bundle Updated", message: "<%= file.relative %>"}))
+                     .on("error", notify.onError({title: "Broserify Error", message: "<%= error.message %>"}))
           })
           .bundle()
-          .on("error", notify.onError({
-            message: "<%= error.message %>",
-            title: "Broserify Error"
-          }))
+          .on("error", notify.onError({title: "Broserify Error", message: "<%= error.message %>"}))
   });
 
   gulp.src(config.src)
+    .pipe(notify({title: "Broserify - Compling Bundld", message: "<%= file.relative %>"}))
     .pipe(browserified)
-    .pipe(rename("bundle.js"))
-    .pipe(gulp.dest(config.dist))
-    .pipe(notify({
-      title: "Broserify Bundle Created",
-      message: "<%= file.relative %>"
-    }));
+    .pipe(rename(destFileName))
+    .pipe(notify({title: "Broserify - Bundle Created", message: "<%= file.relative %>"}))
+    .pipe(gulp.dest(destDir));
+
 });
 
-
-/*
-gulp.task('browserify', function() {
-    //rename output file with .coffee prefix and .js surffix (Doist Convention)
-    destFileName = "bundle.js"
-
-    var bundler = browserify({
-        entries: [config.src],
-        paths: ['../doist_gulp/node_modules'],
-        //if you don't use jsx syntax in your .coffee, you could change it to coffeeify instead
-        debug: true,
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-    });
-
-    var watcher  = watchify(bundler);
-
-    return watcher
-      .on('update', function () { // When any files update
-          console.log('Watchify detect updated. Rebundle...');
-          var updateStart = Date.now();
-
-          watcher.bundle()
-                 .on("error", notify.onError({
-                    message: "<%= error.message %>",
-                    title: "Broserify Error"
-                 }))
-                 .pipe(source(destFileName))
-                 .pipe(gulp.dest(config.dist))
-                 .pipe(notify({
-                    title: "Broserify Bundle Created",
-                    message: "<%= file.relative %>"
-                 }));
-
-          console.log('Rebundle finished!', (Date.now() - updateStart) + 'ms');
-      })
-      .bundle() // Create the initial bundle when starting the task
-      .on("error", notify.onError({
-        message: "<%= error.message %>",
-        title: "Broserify Error"
-      }))
-      .pipe(source(destFileName))
-      .pipe(gulp.dest(config.dist))
-      .pipe(notify({
-        title: "Broserify Bundle Created",
-        message: "<%= file.relative %>"
-      }));
-});
-*/
-
-
-/*
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var notify = require("gulp-notify");
-var debug = require('gulp-debug');
-var rename = require("gulp-rename");
-
-
-
-gulp.task('browserify', function() {
-
-  var browserified = transform(function(filename) {
-    var b = browserify(filename, {
-      paths: ['../doist_gulp/node_modules'],
-      debug: true
-    });
-    return b.bundle();
-  });
-
-  gulp.src(config.src)
-    .pipe(browserified)
-    .pipe(gulp.dest(config.dist))
-    .pipe(coffee({bare: true}))
-    .on('error', 
-      notify.onError( function (error) {
-        return "Coffee compile error: " + error.message;
-      })
-    )
-    .pipe(rename({
-      prefix: ".coffee."
-    }))
-});
-*/
